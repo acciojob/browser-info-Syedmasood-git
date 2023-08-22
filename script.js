3b9457bcb5515715f75d0eb7b8fafb48be2a71ac
SyedMasood46 <masoodhussaini46@gmail.com>
2023-08-22 18:11:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //your JS code here. If required.
 const browserName=navigator.appName;
-const browserversion=navigator.appVersion;
+const browserVersion=navigator.appVersion;
 
 const message="You are using " + browserName + " version " + browserVersion;
 const browserInfo=document.getElementById("browser-info");
